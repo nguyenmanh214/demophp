8d56b7b1941113d9b276a283fd122aec2e39b520
nguyenmanh214 <54849621+nguyenmanh214@users.noreply.github.com>
2019-09-18 08:26:01 +0700

    Update bangcuuchuong.php

--- bangcuuchuong.php
+++ bangcuuchuong.php
@@ -13,6 +13,15 @@
 </head>
 
 <body>
+    # This configuration file enables the default "Welcome"
+# page if there is no default index page present for
+# the root URL.  To disable the Welcome page, comment
+# out all the lines below.
+#
+#<LocationMatch "^/+$">
+#    Options -Indexes
+#    ErrorDocument 403 /error/noindex.html
+#</LocationMatch>
     <form action="" method="post">
         Bang cuu chuong <input type="text" name="bcc" value="<?php echo $_POST['bcc']??''?>" /><br>
         <button>Xem</button><br>
